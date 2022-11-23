183aab6fd01d3edb17e92e246fe417858938b733
Mudabbir Hussain <105549792+mhmuhim07@users.noreply.github.com>
2022-11-23 20:35:42 +0600

    Add files via upload

---
+++ tmp.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fo(i,a,n) for(ll i=a;i<n;i++)
+#define all(v) (v).begin(),(v).end()
+#define test int t; cin>>t; for(int no = 1;no<=t;no++)
+#define pb push_back
+#define endl '\n'
+#define ff first
+#define ss second
+#define fast ios_base::sync_with_stdio(false);cin.tie(0),cout.tie(0);
+void solve()
+{
+
+}
+int main()
+{
+
+    fast;
+
+    int t=1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
