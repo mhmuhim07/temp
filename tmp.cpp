b56ad653d1ee8e25617e5e978f27d49426a5d55e
Mudabbir Hussain <105549792+mhmuhim07@users.noreply.github.com>
2023-01-25 20:29:07 +0600

    Update tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -1,17 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define vi vector<int>
+#define vl vector<ll>
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define srt(v) sort(v.begin(),v.end())
+#define rsrt(v) sort(v.rbegin(),v.rend())
 #define ll long long
 #define fo(i,a,n) for(ll i=a;i<n;i++)
 #define all(v) (v).begin(),(v).end()
-#define test int t; cin>>t; for(int no = 1;no<=t;no++)
+#define rall(v) (v).rbegin(),(v).rend()
 #define pb push_back
 #define endl '\n'
 #define ff first
 #define ss second
 #define fast ios_base::sync_with_stdio(false);cin.tie(0),cout.tie(0);
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+
 void solve()
 {
+    
 
 }
 int main()
