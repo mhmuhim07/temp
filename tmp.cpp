4fe2b00c238489158cc8d27f7036398c7a1c7d7f
Mudabbir Hussain <105549792+mhmuhim07@users.noreply.github.com>
2024-04-18 05:23:42 +0600

    tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -8,6 +8,8 @@ using namespace __gnu_pbds;
  
 template <typename T> using o_set = tree<T, null_type, greater<T>, rb_tree_tag, tree_order_statistics_node_update>;
 template <typename T> using _set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//order_of_key (k) : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).
 
 void solve(int cs){
     int n;
